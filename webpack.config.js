b83221f7dfbfcad445eaedfb180b8b0f0b253ece
Sercasaurus <nills.van.limbeek@live.nl>
2018-02-10 13:33:13 +0100

    added postcss

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ module.exports = {
             'scss': [
               'vue-style-loader',
               'css-loader',
+              'postcss-loader',
               'sass-loader'
             ],
             'sass': [
